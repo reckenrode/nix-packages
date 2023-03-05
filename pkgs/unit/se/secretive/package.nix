43f6a15e2530a44045848d9aee9f1c7e8188cc50
Randy Eckenrode <randy@largeandhighquality.com>
2023-03-05 15:46:14 -0500

    secretive: init at 2.3.0

---
+++ pkgs/unit/se/secretive/package.nix
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: MIT
+
+{ lib
+, fetchurl
+, stdenv
+, unzip
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "secretive";
+  version = "2.3.0";
+
+  src = fetchurl {
+    url = "https://github.com/maxgoedjen/secretive/releases/download/v${finalAttrs.version}/Secretive.zip";
+    hash = "sha256-X8+54irgX6YZcbfFcIn3DTbRZz4A3TlePoWeWFISgqY=";
+  };
+
+  nativeBuildInputs = [ unzip ];
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    cp -R ../*.app $out/Applications
+  '';
+
+  meta = {
+    description = ''
+      Secretive is an app for storing and managing SSH keys in the Secure Enclave. It is inspired by
+      the sekey project, but rewritten in Swift with no external dependencies and with a handy
+      native management app.
+    '';
+    homepage = "https://github.com/maxgoedjen/secretive";
+    changelog = "https://github.com/maxgoedjen/secretive/releases";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.darwin;
+  };
+})
