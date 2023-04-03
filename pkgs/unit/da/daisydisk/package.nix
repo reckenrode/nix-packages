997238fc4de5e80b332c7ba253a1fea4fd120117
Randy Eckenrode <randy@largeandhighquality.com>
2023-04-03 08:29:20 -0400

    daisydisk: init at 4.24

---
+++ pkgs/unit/da/daisydisk/package.nix
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: MIT
+
+{ lib
+, fetchurl
+, stdenv
+, unzip
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "daisydisk";
+  version = "4.24";
+
+  src = fetchurl {
+    url = "https://web.archive.org/web/20230326094956/https://daisydiskapp.com/download/DaisyDisk.zip";
+    hash = "sha256-UWkJ8teqpFBcvU0UujCxLzyQ3bGwC9Zbsh5xWCyQFcc=";
+  };
+
+  nativeBuildInputs = [ unzip ];
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    cp -R ../*.app $out/Applications
+  '';
+
+  meta = {
+    description = ''
+      Find out what’s taking up your disk space and recover it in the most efficient and easy way.
+    '';
+    homepage = "https://daisydiskapp.com";
+    changelog = "https://daisydiskapp.com/releases";
+    # Temporarily until it’s possible to allow unfree flake packages without impurities
+    # license = lib.licenses.unfree;
+    platforms = lib.platforms.darwin;
+  };
+})
