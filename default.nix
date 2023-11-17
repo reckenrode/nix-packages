2bc4c120333f8a80997162f01e27097d525e2e7e
Randy Eckenrode <randy@largeandhighquality.com>
2023-11-17 00:12:29 -0500

    Revert "Drop default.nix (use nix-update instead)"
    This reverts commit b11fa79fc351407c1102195e7a48bbd972f9d63d.

---
+++ default.nix
@@ -0,0 +1,10 @@
+# This `default.nix` is provided only for compatibility with `update-source-version`.
+# It blindly overlays the flake’s packages over `<nixpkgs>`. Use at your own risk.
+let
+  flakePackages = (builtins.getFlake (toString ./.)).outputs.packages;
+in
+import <nixpkgs> {
+  overlays =  [
+    (self: super: flakePackages.${builtins.currentSystem})
+  ];
+}
