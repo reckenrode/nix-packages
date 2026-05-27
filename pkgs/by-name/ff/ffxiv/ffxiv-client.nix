55e57f97f6d6be1758a4d06f7634bb5d0e7715d4
Randy Eckenrode <randy@largeandhighquality.com>
2026-05-26 20:56:32 -0400

    ffxiv: 1.1.10 -> 1.1.11

--- pkgs/by-name/ff/ffxiv/ffxiv-client.nix
+++ pkgs/by-name/ff/ffxiv/ffxiv-client.nix
@@ -9,14 +9,14 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "ffxiv-client";
-  version = "1.1.10";
+  version = "1.1.11";
 
   # The Windows installer is a 32-bit app, which won’t run on Darwin because WoW64 is not yet
   # supported there with upstream Wine. The Mac client also has Bink-encoded video files that are
   # needed because the WMV-encoded ones in the Windows client don’t work with Wine by default.
   src = fetchurl {
     url = "https://mac-dl.ffxiv.com/cw/finalfantasyxiv-${finalAttrs.version}.zip";
-    hash = "sha256-+LWUugXUbJAhbPCo49ZNpBlby4g0Ro98BUVCmthozIw=";
+    hash = "sha256-p40YFxwRkSEXd4q7alsB5/6MDmOb76sorqOeNyqM11s=";
   };
 
   nativeBuildInputs = [ unzip ];
